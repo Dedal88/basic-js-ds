95773c7bf12ab3feb75ae9428f5c9d87166fe530
Dedal88 <audit37msv@yandex.ru>
2024-12-29 18:42:09 +0300

    fix: changing the code to pass the test successfully

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,108 +1,101 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+//const { Node } = require('../extensions/list-tree.js');
 
 /**
  * Implement simple binary search tree according to task description
  * using Node from extensions
  */
 
-// class NodeTree {
-//   constructor(data) {
-//     this.value = data;
-//     this.left = null;
-//     this.right = null;
-//   }
-// }
 class BinarySearchTree {
   constructor() {
-    this.root = null;
+    this.rootTree = null;
   }
 
   root() {
-    if (!this.root) {
-      return;
-    }
-    return this.root;
+    // if (!this.rootTree) {
+    //   return null;
+    // }
+    return this.rootTree;
   }
 
   add(data) {
-    this.root = addWithin(this.root, data);
+    this.rootTree = addWithin(this.rootTree, data);
 
-    function addWithin(node, value) {
+    function addWithin(node, data) {
       if (!node) {
         return {
-          value: value,
+          data: data,
           left: null,
           right: null,
         };
       }
 
-      if (node.value === value) {
+      if (node.data === data) {
         return node;
       }
 
-      if (value < node.value) {
-        node.left = addWithin(node.left, value);
+      if (data < node.data) {
+        node.left = addWithin(node.left, data);
       } else {
-        node.right = addWithin(node.right, value);
+        node.right = addWithin(node.right, data);
       }
       return node;
     }
   }
 
   has(data) {
-    return searchWithin(this.root, data);
+    return searchWithin(this.rootTree, data);
 
-    function searchWithin(node, value) {
+    function searchWithin(node, data) {
       if (!node) {
         return false;
       }
 
-      if (node.value === value) {
+      if (node.data === data) {
         return true;
       }
 
-      return value < node.value
-        ? searchWithin(node.left, value)
-        : searchWithin(node.right, value);
+      return data < node.data
+        ? searchWithin(node.left, data)
+        : searchWithin(node.right, data);
     }
   }
 
   find(data) {
-    return findWithin(this.root, data);
+    return findWithin(this.rootTree, data);
 
-    function findWithin(node, value) {
+    function findWithin(node, data) {
       if (!node) {
         return null;
       }
 
-      if (node.value === value) {
+      if (node.data === data) {
         return node;
       }
 
-      return value < node.value
-        ? findWithin(node.left, value)
-        : findWithin(node.right, value);
+      return data < node.data
+        ? findWithin(node.left, data)
+        : findWithin(node.right, data);
     }
   }
 
   remove(data) {
-    this.root = removeNode(this.root, data);
+    this.rootTree = removeNode(this.rootTree, data);
 
-    function removeNode(node, value) {
+    function removeNode(node, data) {
       if (!node) {
         return null;
       }
 
-      if (value < node.value) {
-        node.left = removeNode(node.left, value);
+      if (data < node.data) {
+        node.left = removeNode(node.left, data);
         return node;
-      } else if (node.value < value) {
-        node.right = removeNode(node.right, value);
+      } else if (node.data < data) {
+        node.right = removeNode(node.right, data);
         return node;
       } else {
-        // value equal
+        // data equal
 
         //maybe node is a leaf
         if (!node.left && !node.right) {
@@ -125,8 +118,8 @@ class BinarySearchTree {
           minFromRight = minFromRight.left;
         }
 
-        node.value = minFromRight.value;
-        node.right = removeNode(node.right, minFromRight.value);
+        node.data = minFromRight.data;
+        node.right = removeNode(node.right, minFromRight.data);
 
         return node;
       }
@@ -134,27 +127,27 @@ class BinarySearchTree {
   }
 
   min() {
-    if (!this.root) {
+    if (!this.rootTree) {
       return;
     }
 
-    let node = this.root;
+    let node = this.rootTree;
     while (node.left) {
       node = node.left;
     }
-    return node.value;
+    return node.data;
   }
 
   max() {
-    if (!this.root) {
+    if (!this.rootTree) {
       return;
     }
 
-    let node = this.root;
+    let node = this.rootTree;
     while (node.right) {
       node = node.right;
     }
-    return node.value;
+    return node.data;
   }
 }
 
